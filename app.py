bc41052a0963b0e4f0b86065f44b9df9d3a890f8
studygianella-netizen <studygianella@gmail.com>
2026-06-12 13:42:07 -0500

    Refactor app.py for new project configuration

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
-
-st.set_page_config(page_title="Proyecto 1 Python", layout="wide")
+# =====================================================
+# CONFIGURACIÓN DE LA APLICACIÓN
+# =====================================================
+st.set_page_config(
+    page_title="Bank Marketing Analytics",
+    page_icon="📊",
+    layout="wide")
 
 st.sidebar.image("python.png")
 
-st.title("PROYECTO 1")
-
-st.sidebar.title("Parámetros")
+st.title("PROYECTO 2")
 
 st.write("Elaborado por: Gianella Zorrilla Quispe")
 st.write("Carrera: Administración de empresas")
@@ -15,50 +18,13 @@ st.write ("Curso: Especialización en Python potenciado con IA - Edición - 58")
 st.write ("Año: 2026")
 st.image("fondo.png")
 
-menu = st.sidebar.selectbox("Seleccione una opción",
-    ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
 
+st.sidebar.title("📌 Menú Principal")
+opcion = st.sidebar.radio(
+    "Navegación",
+    ["🏠 Home"])
 # =====================================================
 # HOME
 # =====================================================
 
-if menu == "Home":
-    st.write("Bienvenido a mi aplicación")
-    st.write("Descripción del proyecto:")
-    st.write("Aplicación interactiva desarrollada en Streamlit como proyecto aplicado del módulo de Fundamentos de Programación. El proyecto integra conceptos esenciales de Python como variables, estructuras de datos, control de flujo, funciones, programación funcional y programación orientada a objetos (POO), mediante una interfaz dinámica e intuitiva.")
-    st.write("Tecnologías utilizadas:")
-    st.write("""
-    - Python
-    - Streamlit
-    - Pandas
-    - NumPy
-    """)
 
-# =====================================================
-# EJERCICIO 1
-# =====================================================
-
-elif menu == "Ejercicio 1":
-    
-# =====================================================
-# EJERCICIO 2
-# =====================================================
-
-elif menu == "Ejercicio 2":
-   
-
-    
-# =====================================================
-# EJERCICIO 3
-# =====================================================
-
-elif menu == "Ejercicio 3":
-   
-
-
-# =====================================================
-# EJERCICIO 3
-# =====================================================
-    
-elif menu == "Ejercicio 4":
-   
